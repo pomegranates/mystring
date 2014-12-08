e059ae58f55659a3168f5bea7c33cd38b008afcf
Vulliard Loan <loan.vulliard@insa-lyon.fr>
2014-12-08 17:15:51 +0100

    Added the resize method

--- MyString.cpp
+++ MyString.cpp
@@ -23,7 +23,7 @@
 
 //############################################################################
 //                                                                           #
-//                              Class    MyString                            #
+//                                Class  MyString                            #
 //                                                                           #
 //############################################################################
 
@@ -50,6 +50,91 @@ MyString::~MyString(void)
 // ===========================================================================
 //                                 Public Methods
 // ===========================================================================
+void MyString::resize(size_t n)
+{
+	//First case, if we want to broaden the string
+	if(n > mem_length)
+	{
+ 		char* temp_chars = new char(n);
+	 	int i;
+	 	for(i=0;i<n;i++)
+	 	{
+	 		//Copy existing characters
+	 		if(i<mem_length)
+	 		{
+				temp_chars[i]=chars[i];
+	 		}
+	 		//Complete with null characters
+	 		else
+	 		{
+	 			temp_chars[i]='\0';
+	 		}
+	 	}
+	 	//Replace the object characters by the new ones
+	 	delete [] chars;
+	 	chars = temp_chars;
+ 	}
+
+ 	//If we want to shrink the string
+	if(n < mem_length)
+	{
+		char* temp_chars = new char(n);
+	 	int i;
+	 	//Copy the first n characters
+	 	for(i=0;i<n;i++)
+	 	{
+			temp_chars[i]=chars[i];
+	 	}
+	 	//Replace the object characters by the new ones
+	 	delete [] chars;
+	 	chars = temp_chars;
+	}
+
+	mem_length= (int) n;
+}
+
+void MyString::resize(size_t n, char c)
+{
+	//First case, if we want to broaden the string
+	if(n > mem_length)
+	{
+ 		char* temp_chars = new char(n);
+	 	int i;
+	 	for(i=0;i<n;i++)
+	 	{
+	 		//Copy existing characters
+	 		if(i<mem_length)
+	 		{
+				temp_chars[i]=chars[i];
+	 		}
+	 		//Complete with c characters
+	 		else
+	 		{
+	 			temp_chars[i]=c;
+	 		}
+	 	}
+	 	//Replace the object characters by the new ones
+	 	delete [] chars;
+	 	chars = temp_chars;
+ 	}
+
+ 	//If we want to shrink the string
+	if(n < mem_length)
+	{
+		char* temp_chars = new char(n);
+	 	int i;
+	 	//Copy the first n characters
+	 	for(i=0;i<n;i++)
+	 	{
+			temp_chars[i]=chars[i];
+	 	}
+	 	//Replace the object characters by the new ones
+	 	delete [] chars;
+	 	chars = temp_chars;
+	}
+
+	mem_length= (int) n;
+}
 
 // ===========================================================================
 //                                Protected Methods
